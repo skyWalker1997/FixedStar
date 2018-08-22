a0aa005960bcc9ebdacfd596dab50051e2de040b
CN_skywalker <1004268134@qq.com>
2018-08-22 15:45:53 +0800

    Last update in XML version

--- Spiders/ThatsThem/test.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-from Spiders.ThatsThem.util import DB_util
-
-portnumb = 27017
-hostname = "localhost"
-DataBaseName = "Spider"
-CollectionName = "ThatsThem"
-conn = DB_util.MongoDBConnect(DataBaseName, CollectionName, hostname, portnumb)
-DB_util.MongoDBAdd("1", "1", "1", "1", "1", "1", conn)
